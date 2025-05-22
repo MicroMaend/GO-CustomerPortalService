d94dbba768154fbba1a575347b4ada82688739e2
noahrdam <noahrdam@gmail.com>
2025-05-22 10:17:33 +0200

    program.cs test

--- GO-CustomerPortalService/Program.cs
+++ GO-CustomerPortalService/Program.cs
@@ -8,7 +8,8 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped(sp =>
+    new HttpClient { BaseAddress = new Uri("/api/") });
 
 builder.Services.AddBlazoredLocalStorage();
 builder.Services.AddScoped<IAuctionService, AuctionHttpService>();
