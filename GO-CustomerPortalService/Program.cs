39dc655530b990874c7f9a3dd94a60a846f2ea1b
Gabrieldarri <gabrielbjarnason@gmail.com>
2025-05-26 10:49:46 +0200

    tilbage fra start

--- GO-CustomerPortalService/Program.cs
+++ GO-CustomerPortalService/Program.cs
@@ -2,9 +2,6 @@ using GO_CustomerPortalService;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Blazored.LocalStorage;
-using Microsoft.AspNetCore.Components.Authorization;
-using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
-using Microsoft.Extensions.DependencyInjection;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -18,26 +15,5 @@ builder.Services.AddScoped<IBiddingService, BiddingHttpService>();
 builder.Services.AddScoped<ICatalogService, CatalogHttpService>();
 builder.Services.AddScoped<IUserService, UserHttpService>();
 
-builder.Services.AddAuthorizationCore();
-
-// Konfigurer autentificering ved hjælp af OidcAuthentication
-builder.Services.AddOidcAuthentication(options =>
-{
-    options.ProviderOptions.Authority = "/";
-    options.ProviderOptions.MetadataUrl = "_configuration/.well-known/openid-configuration";
-    options.ProviderOptions.ClientId = "blazorapp";
-    options.ProviderOptions.ResponseType = "code";
-
-    options.ProviderOptions.DefaultScopes.Clear();
-    options.ProviderOptions.DefaultScopes.Add("openid");
-    options.ProviderOptions.DefaultScopes.Add("profile");
-
-    options.AuthenticationPaths.LogInPath = "/login";
-    options.AuthenticationPaths.LogInCallbackPath = "/authentication/login-callback";
-    options.AuthenticationPaths.LogOutPath = "/logout";
-    options.AuthenticationPaths.LogOutCallbackPath = "/authentication/logout-callback";
-    options.AuthenticationPaths.RegisterPath = "/register";
-
-});
 
 await builder.Build().RunAsync();
