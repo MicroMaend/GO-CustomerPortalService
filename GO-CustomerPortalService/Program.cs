17b1a8ea187c2e0da09f4dadf1da85ab996ead1d
Gabrieldarri <gabrielbjarnason@gmail.com>
2025-05-26 10:10:51 +0200

    tilbage til noget som vriker thoughts

--- GO-CustomerPortalService/Program.cs
+++ GO-CustomerPortalService/Program.cs
@@ -2,9 +2,6 @@ using GO_CustomerPortalService;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Blazored.LocalStorage;
-using Microsoft.AspNetCore.Components.Authorization; 
-using GO_CustomerPortalService.Authentication;
-
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -18,12 +15,5 @@ builder.Services.AddScoped<IBiddingService, BiddingHttpService>();
 builder.Services.AddScoped<ICatalogService, CatalogHttpService>();
 builder.Services.AddScoped<IUserService, UserHttpService>();
 
-// Tilføj din CustomAuthStateProvider
-builder.Services.AddScoped<AuthenticationStateProvider, CustomAuthStateProvider>();
-
-//thoughts?
-
-// Tilføj AuthorizationCore
-builder.Services.AddAuthorizationCore();
 
 await builder.Build().RunAsync();
